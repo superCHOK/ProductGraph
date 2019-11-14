d034d7d33a0ca1ffcd6672fd91e0a7101d911ce5
superchok <super.chok@hotmail.com>
2019-11-15 02:01:07 +0700

    ulmfit

--- word_token.py
+++ word_token.py
@@ -2,9 +2,11 @@
 from pythainlp import word_tokenize
 from pythainlp.corpus import thai_stopwords
 from pythainlp.corpus import wordnet
+from pythainlp.ulmfit import *
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import words
 from stop_words import get_stop_words
+from fastai.text import *
 
 import re
 import string
@@ -17,7 +19,9 @@ p_stemmer = PorterStemmer()
 def clean_msg(msg):
     
     # ลบ text ที่อยู่ในวงเล็บ <> 
-    msg = re.sub(r'<.*?#>','', msg)
+    msg = re.sub(r'<.*#?>','', msg)
+    # ลบ hashtag
+    msg = re.sub(r'#','',msg)
     
     # ลบ เครื่องหมายคำพูด 
     for c in string.punctuation:
@@ -29,7 +33,7 @@ def clean_msg(msg):
     return msg
 
 def split_word(text):
-    words = word_tokenize(text_clean,engine='newmm')
+    words = word_tokenize(text,engine='newmm')
 
     # Remove stop words TH and EN
     words = [i for i in words if not i in th_stop and not i in en_stop]
@@ -56,11 +60,16 @@ def split_word(text):
 
 
 text_list = ["#อีกหนึ่งในความทันสมัยเพิ่มขึ้นด้วยที่กรองใบชา และเศษเครื่องดื่ม ที่อยู่ส่วนบนปากของกา ตัวเครื่องผลิตจากสแตนเลส ไม่เป็นสนิม ทำให้น้ำร้อนได้เร็วทันใจฝาเปิดค้างไว้ได้เพียงกดปุ่ม และกดฝาให้ลงล็อคเมื่อปิดฝา ตัวฐานหมุนรอบได้ 360 องศา ยกตัวกาออกจากฐานได้จึงทำให้สะดวก เหมาะสำหรับการพกพาไปใช้ในสถานที่ต่าง ๆ อีกทั้งยังมีขีดบอกระดับน้ำที่ดูได้ง่าย มีไฟแสดงสถานะความร้อน พร้อมระบบตัดไฟฟ้าอัตโนมัติเมื่อน้ำเดือดแล้ว หรือมีความร้อนสูง จึงมีความปลอดภัยยิ่งขึ้น", "คุณสมบัติต้มน้ำร้อนได้เร็ว ประมาณ 4 นาที ตัวเครื่องทำจากสแตนเลส ไม่เป็นสนิมฮีตเตอร์แบบขดลวด ทำจากสแตนเลสระบบตัดไฟอัตโนมัติเมื่อน้ำเดือด หรือน้ำแห้งมีที่กรองเศษใบชา และกากเครื่องดื่ม ตรงส่วนปากของกาเหมาะอย่างยิ่งสำหรับใช้ในห้องพักโรงแรม หรือพกพาท่องเที่ยวตัวกาสามารถแยกออกจากฐานได้ตัวฐานหมุนได้รอบ 360 องศาความจุ 1.8 ลิตรขนาด กว้าง 17 ซม. (วัดจากใต้ฐาน) * สูง 23 ซม.(รวมฐาน)/ สูง 20.5 ซม.(ไม่รวมฐาน)กำลังไฟ 1500w 220v 50Hz"]
-
+text_clean = []
 text_clean = [clean_msg(text) for text in text_list]
 print(text_clean)
 
-proc = word_tokenize(text_list[0], engine='newmm')
-print(proc)
-print(len(proc))
+vect = document_vector(text_clean[0],)
+print(vect)
+
+words = [split_word(text) for text in text_clean]
+print(words)
+
+
+
 
