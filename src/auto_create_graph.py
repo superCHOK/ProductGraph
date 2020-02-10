4544efe89b09b2e60aeb9bd90b223e9df52e7570
superchok <super.chok@hotmail.com>
2020-02-10 22:40:14 +0700

    upload files

--- src/auto_create_graph.py
+++ src/auto_create_graph.py
@@ -22,27 +22,8 @@ from stop_words import get_stop_words
 import webbrowser
 from py2neo import Node, Relationship, Graph
 
-file_name = 'category1'
-
-FILE = './Data/{}.csv'.format(file_name)
-df = pd.read_csv(FILE)
-
-# Drop a column that not use
-data = df.drop(columns=['Unnamed: 0'])
-
-# Drop column that all equal to NaN
-data = data.dropna(axis=1, how='all')
-
-# Fill NaN with "-"
-data = data.fillna('-')
-
-file_name2 = './Data/attributes_category1.txt'
-with open(file_name2, 'r', encoding='utf-8') as f:
-    attr_fix = f.read().splitlines()
-
-regex_attr = ("|").join(attr_fix)
 # clean the text and prepre some attribute
-def clean_text_category1(msg):
+def clean_text_category1(msg, attr_fix):
     msg = str(msg).lower()
     msg = re.sub(r"smartphone[s]?","สมาร์ทโฟน",str(msg))
     msg = re.sub(r"computers และ laptops","คอมพิวเตอร์ และ แล็ปท็อป",str(msg))
@@ -57,8 +38,8 @@ def clean_text_category1(msg):
         msg = re.sub(r"(คอมพิวเตอร์ และ แล็ปท็อป)",r"หมวดหมู่ \1",str(msg))
     elif (str(msg)=='แล็ปท็อป') | (str(msg)=='แล็ปท็อปสำหรับเล่นเกมส์'):
         msg = re.sub(r"(แล็ปท็อป|แล็ปท็อปสำหรับเล่นเกมส์)",r"ประเภทย่อย \1",str(msg))
-    elif (str(msg)=='คอมพิวเตอร์แบบตั้งโต๊ะ') | (str(msg)=='gaming desktops'):
-        msg = re.sub(r"(คอมพิวเตอร์แบบตั้งโต๊ะ|gaming desktops)",r"ประเภทย่อย \1",str(msg))
+    elif (str(msg)=='คอมพิวเตอร์แบบตั้งโต๊ะ') | (str(msg)=='gaming desktops') | (str(msg)=='คอมประกอบ'):
+        msg = re.sub(r"(คอมประกอบ|คอมพิวเตอร์แบบตั้งโต๊ะ|gaming desktops)",r"ประเภทย่อย \1",str(msg))
     elif re.match("[0-9]\*[0-9]",str(msg)):
         msg = re.sub(r"\*","x",str(msg))
     
@@ -69,9 +50,12 @@ def clean_text_category1(msg):
     msg = re.sub(r"model","โมเดล ",str(msg))
     msg = re.sub(r"กราฟฟิก|กราฟิก","graphics",str(msg))
     msg = re.sub(r"^color|^colour","สี ",str(msg))
-    msg = re.sub(r"พาวเวอร์ซัพพลาย\s","power supply ",str(msg))
-    msg = re.sub(r"เมนบอร์ด\s","mainboard ",str(msg))
+    msg = re.sub(r"พาวเวอร์ซัพพลาย\s|power\ssupply\s","psu ",str(msg))
+    msg = re.sub(r"เมนบอร์ด\s|^mb\s","mainboard ",str(msg))
     msg = re.sub(r"warranty\speriod\s+?","ระยะเวลาการรับประกัน ",str(msg))
+    msg = re.sub(r"\sprocessor\s","cpu ",str(msg))
+    msg = re.sub(r"เลโนโว่"," lenovo ",str(msg))
+    msg = re.sub(r"ซัมซุง"," samsung ",str(msg))
     
     msg = re.sub(r"จอแสดงผล\s(size|screen)|[\W]จอแสดงผล\s|^screen\s|^monitor\s","หน้าจอแสดงผล ",str(msg))
     msg = re.sub(r"^resolution\s+?","ความละเอียด ",str(msg))
@@ -91,19 +75,20 @@ def clean_text_category1(msg):
     msg = re.sub(r"\w*\s?dimension","ขนาด ",str(msg))
     msg = re.sub(r"พอร์ต|ports\s","port ",str(msg))
     
-    msg = re.sub(r"^(amd ryzen|intel\w*i)",r"cpu \1",str(msg))
-    msg = re.sub(r"^(nvidia|intel\w*graphic)",r"gpu \1",str(msg))
-    msg = re.sub(r"^(\d+\.?\d?\sinch)",r"ขนาดหน้าจอ \1",str(msg))
-    msg = re.sub(r"^(windows)",r"ระบบปฏิบัติการ \1",str(msg))
-    msg = re.sub(r"\s(\d+\s)month[s]?",r"\1 เดือน",str(msg))
-    msg = re.sub(r"\s(\d+\s)year[s]?",r"\1 ปี",str(msg))
-    
     # หน่วย
     msg = re.sub(r"\฿,"," บาท ",str(msg))
     msg = re.sub(r"\sมิลลิเมตร|\sมม\.?|millimeter\.?"," mm ",str(msg))
     msg = re.sub(r"\sเซนติเมตร|\sซม\.?|centimeter\.?"," cm ",str(msg))
     msg = re.sub(r"\sกิโลกรัม|\sกก\.?|kilogram[s]?\.?"," kg ",str(msg))
     msg = re.sub(r"\sนิ้ว"," inch ",str(msg))
+    
+    msg = re.sub(r"^(amd ryzen|intel\w*[ip])",r"cpu \1",str(msg))
+    msg = re.sub(r"^(nvidia|intel\w*graphic)",r"gpu \1",str(msg))
+    msg = re.sub(r"^(\d+\.?\d?\sinch)",r"ขนาดหน้าจอ \1",str(msg))
+    msg = re.sub(r"^(windows)",r"ระบบปฏิบัติการ \1",str(msg))
+    msg = re.sub(r"\s(\d+\s)month[s]?",r"\1 เดือน",str(msg))
+    msg = re.sub(r"\s(\d+\s)year[s]?",r"\1 ปี",str(msg))
+    
     
     # clean special character
     msg = re.sub(r"[\,]","",str(msg))
@@ -130,127 +115,65 @@ def clean_name(msg):
     return msg
     
 # function for defining attribute of product
-def define_attr(txt):
+def define_attr(txt,regex_attr):
     p = re.compile('(หมวดหมู่|ประเภทย่อย|{}|)'.format(regex_attr), re.VERBOSE)
     #match = re.match(r"", str(txt))
     txt = re.sub(p,r"|\1|",str(txt))
     
     return txt
 
-# Cleaning text with clean_text function and keep in dict
-data_clean = {}
-name_item = {}
-for key,value in data.iteritems():
-    tmp = []
-    for i,txt in enumerate(value):
-        if not txt == '-':
-            if i==2 and re.match(r"category_[01]_\d+",str(key)):
-                name_item[key] = clean_name(txt)
-            elif i==3 and re.match(r"category_[2345678]_\d+",str(key)):
-                name_item[key] = clean_name(txt)
-            else:
-                tmp.append(clean_text_category1(txt))
-    
-    tmp = [i for i in tmp if i] # Clear None value
-    data_clean[key] = tmp 
-
-# Mark attribute with '|'
-attr_mark = {}
-for key,value in data_clean.items():
-    tmp = [define_attr(txt) for txt in value] 
-    tmp = [re.sub(r"\|\|","",str(t)) for t in tmp]
-    tmp = [i for i in tmp if re.search(r"\|",i)] 
-    attr_mark[key] = tmp 
-    
 # Split attribute
 def cap_attr(text):
     text = text.split("|")
     
     return text
 
-# Split attribute
-def cap_attr(text):
-    text = text.split("|")
-    
-    return text
 
-# Construct triples of product
-triples = []
-for i,v in attr.items():
-    for l in v:
-        idx=0
-        for idx in range(len(l)-1):
-            tmp_list = [i]
-            if l[idx] in attr_fix:
-                tmp_list.extend([l[idx],l[idx+1]])
-                triples.append(tmp_list)
-            idx += 1     
-            
-# Write the triples to text file
-with open('./Data/triples_{}.txt'.format(file_name), 'w', encoding='utf-8') as f:
-    for _list in triples:
-        for _string in _list:
-            #f.seek(0)
-            f.write(str(_string) + '\t')
-        f.write('\n')
-        
-# Product name and index
-with open('./Data/Product_names_{}.txt'.format(file_name), 'w', encoding='utf-8') as fn:
-    print(name_item, file=fn)
-    
-# Read triples from text file
-with open ("Data/triples_01.txt", "r", encoding='utf-8') as myfile:
-    triples = myfile.read().splitlines()
-for i in range(len(triples)):
-    triples[i] = triples[i].split('\t')
-    triples[i] = [m for m in triples[i] if not m =='']
-    
-with open('./Data/Product_names.txt', 'r', encoding='utf-8') as fn: 
-    content = fn.read() 
-    dic = eval(content)
-    
-## Creating Nodes
-nodes_e = []
-nodes_c = []
-relationships = []
-for i in range(len(triples)):
-    product = 0
-    for n in nodes_e:
-        if dict(n)['name'] == dic[triples[i][0]]:
-            product = n
-            break
-    if product==0:
-        product = Node('Product',name = dic[triples[i][0]])
-        nodes_e.append(product)
-    
-    attr = 0
-    for n in nodes_c:
-        if dict(n)['name'] == triples[i][2]:
-            attr = n 
-            break
-    if attr==0:
-        attr = Node('Attribute',name = triples[i][2])
-        nodes_c.append(attr)  
-    
-    relationship = Relationship(product,triples[i][1],attr)
-    relationships.append(relationship)
-    
-# Creating the graph
-graph =Graph(host='localhost',user='neo4j', password='5678') # initial graph from the data
-#graph = Graph('bolt://neo4j:test@127.0.0.1:7687/db/data')
-tx = graph.begin()
 
-for node in nodes_e:
-    tx.create(node)
-for node in nodes_c:
-    tx.create(node)
-for relationship in relationships:
-    tx.create(relationship)
+def create_graph(triples,dic,host,user,password):
+    ## Creating Nodes
+    nodes_e = []
+    nodes_c = []
+    relationships = []
+    for i in range(len(triples)):
+        product = 0
+        for n in nodes_e:
+            if dict(n)['name'] == dic[triples[i][0]]:
+                product = n
+                break
+        if product==0:
+            product = Node('Product',name = dic[triples[i][0]])
+            nodes_e.append(product)
     
-tx.commit()
+        attr = 0
+        for n in nodes_c:
+            if dict(n)['name'] == triples[i][2]:
+                attr = n 
+                break
+        if attr==0:
+            attr = Node('Attribute',name = triples[i][2])
+            nodes_c.append(attr)  
+    
+        relationship = Relationship(product,triples[i][1],attr)
+        relationships.append(relationship)
+    
+    # Creating the graph
+    graph =Graph(host=host,user=user, password=password) # initial graph from the data
+    #graph = Graph('bolt://neo4j:test@127.0.0.1:7687/db/data')
+    tx = graph.begin()
 
-url = 'http://localhost:7474'
-webbrowser.open(url, new=2) # new=2 opens a new tab
+    for node in nodes_e:
+        tx.create(node)
+    for node in nodes_c:
+        tx.create(node)
+    for relationship in relationships:
+        tx.create(relationship)
+    
+    tx.commit()
+
+    url = 'http://localhost:7474'
+    webbrowser.open(url, new=2) # new=2 opens a new tab
+    return
 
 
 
--- src/main.py
+++ src/main.py
@@ -5,6 +5,20 @@ from app import app
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
+import pandas as pd
+import io
+import csv
+import re
+import string
+import nltk
+from auto_create_graph import *
+
+from nltk.corpus import words
+from stop_words import get_stop_words
+
+import webbrowser
+from py2neo import Node, Relationship, Graph
+
 ALLOWED_EXTENSIONS = set(['csv'])
 
 def allowed_file(filename):
@@ -17,9 +31,9 @@ def upload_form():
 @app.route('/', methods=['POST'])
 def upload_file():
 	if request.method == 'POST':
-        # check if the post request has the file part
+		# check if the post request has the file part
 		if 'file' not in request.files:
-			flash('No file part')
+			flash('No file')
 			return redirect(request.url)
 		file = request.files['file']
 		if file.filename == '':
@@ -27,12 +41,92 @@ def upload_file():
 			return redirect(request.url)
 		if file and allowed_file(file.filename):
 			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File successfully uploaded')
+			#file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+			# input
+			df = pd.read_csv(file)
+
+			file_name2 = os.path.join(app.root_path, 'data/attributes_category1.txt')
+
+		
+			with open(file_name2, 'r', encoding='utf-8') as f:
+				attr_fix = f.read().splitlines()
+
+			# Drop a column that not use
+			data = df.drop(columns=['Unnamed: 0'])
+			# Drop column that all equal to NaN
+			data = data.dropna(axis=1, how='all')
+			# Fill NaN with "-"
+			data = data.fillna('-')
+			# Filter
+			data = data.filter(regex ='category_[0-9]_[0-9]+')
+			regex_attr = ("|").join(attr_fix)
+
+			# Cleaning text with clean_text function and keep in dict
+			data_clean = {}
+			name_item = {}
+			for key,value in data.iteritems():
+				tmp = []
+				for i,txt in enumerate(value):
+					if not txt == '-':
+						if i==2 and re.match(r"category_[01]_\d+",str(key)):
+							name_item[key] = clean_name(txt)
+						elif i==3 and re.match(r"category_[23456789]_\d+",str(key)):
+							name_item[key] = clean_name(txt)
+						else:
+							tmp.append(clean_text_category1(txt,attr_fix))
+				tmp = [i for i in tmp if i] # Clear None value
+				data_clean[key] = tmp
+
+			# Mark attribute with '|'
+			attr_mark = {}
+			for key,value in data_clean.items():
+				tmp = [define_attr(txt,regex_attr) for txt in value]
+				tmp = [re.sub(r"\|\|","",str(t)) for t in tmp]
+				tmp = [i for i in tmp if re.search(r"\|",i)]
+				attr_mark[key] = tmp 
+
+			# capture attribute by split the marker
+			attr = {}
+			for i,v in attr_mark.items():
+				tmp_list = []
+				for txt in v:
+					match = cap_attr(txt)
+					match = [m for m in match if not m =='' and not m == ' ']
+					tmp_list.append(match)
+				attr[i] = tmp_list
+
+			# Construct triples of product
+			triples = []
+			for i,v in attr.items():
+				for l in v:
+					idx=0
+					for idx in range(len(l)-1):
+						tmp_list = [i]
+						if l[idx] in attr_fix:
+							tmp_list.extend([l[idx],l[idx+1]])
+							triples.append(tmp_list)
+						idx += 1 
+
+			# Write the triples to text file
+			with open('./data/triples_{}.txt'.format(filename), 'w', encoding='utf-8') as f:
+				for _list in triples:
+					for _string in _list:
+						#f.seek(0)
+						f.write(str(_string) + '\t')
+						f.write('\n')
+
+			# Write product name and index
+			with open('./data/Product_names_{}.txt'.format(filename), 'w', encoding='utf-8') as fn:
+				print(name_item, file=fn)
+
+			create_graph(triples,name_item,'localhost','neo4j','1234')
+
+			flash('Completed!')
 			return redirect('/')
 		else:
-			flash('Allowed file types are csv')
+			flash('Allowed file type is csv')
 			return redirect(request.url)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
